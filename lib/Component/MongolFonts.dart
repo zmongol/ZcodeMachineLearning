28f3822f38ff3da77241e1e341ab973c9dab3502
chuck <tsog.namjil@gmail.com>
2021-03-31 21:11:25 +0800

    Updated the fonts

--- lib/Component/MongolFonts.dart
+++ lib/Component/MongolFonts.dart
@@ -7,17 +7,33 @@ class MongolFonts {
   static final String z52hawangtig = 'z52hawangtig';
   static final String z52qingmingtig = 'z52qingmingtig';
   static final String z52solibotig = 'z52solibotig';
+  static final String z52sonintig = 'z52sonintig';
+  static final String z52ordostig = 'z52ordostig';
+  static final String z52sayatig = 'z52sayatig';
+  static final String z52sanstig = 'z52sanstig';
+  static final String z52holsnujugtig = 'z52holsnujugtig';
+  static final String z52uljitig = 'z52uljitig';
+  static final String z52togtig = 'z52togtig';
+  static final String z52qimedtig = 'z52qimedtig';
   static final String z52tsagaantig = 'z52tsagaantig';
 
   static final List fontList = [
+    [z52ordostig, ' ᡥᡭᢞᢙᡭᢓ ᢘᡬᡬᡨ'],
+    [z52chimegtig, ' ᢚᡬᢎᡪᡬᡨ ᢘᡬᡬᡨ'],
+    [z52qimedtig, ' ᢚᡬᢎᡪᡭᡧ ᢘᡬᡬᡨ'],
+    [z52sanstig, ' ᢔᡪᡪᢓ ᢘᡬᡬᡨ'],
+    [z52sayatig, ' ᢔᡪᡫᡨ ᢘᡬᡬᡨ'],
+    [z52holsnujugtig, ' ᡸᡭᢑᡭᢔᡭᡧ ᡥᡭᡬᢛᡭᡬᡨ ᢘᡬᡬᡨ'],
+    [z52uljitig, ' ᡥᡭᡬᢑᢛᡪᡫ ᢘᡬᡬᡨ'],
     [haratig, ' ᡸᡪᢝᡨ ᢘᡬᡬᡨ'],
     [z52amglangtig, ' ᡥᡪᢎᡭᡱᡱᡭᢑᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
     [z52buduntig, ' ᡴᡭᡬᢙᡭᢋᡭᡧ ᢘᡬᡬᡨ'],
-    [z52chimegtig, ' ᢚᡬᢎᡪᡬᡨ ᢘᡬᡬᡨ'],
     [z52egulentig, ' ᡥᢋᡭᢑᡪᡧ ᢘᡬᡬᡨ'],
     [z52hawangtig, ' ᡸᡪᢟᡪᡪᡬᡨ ᢘᡬᡬᡨ'],
     [z52qingmingtig, ' ᢚᡬᡪᡬᡨ ᢌᡬᡪᡬᡨ ᢘᡬᡬᡨ'],
     [z52solibotig, ' ᢔᡭᢑᡳᡬᡫᡨ  ᢘᡬᡬᡨ'],
+    [z52sonintig, ' ᢔᡭᡱᡬᡧ ᢘᡬᡬᡨ'],
+    [z52togtig, ' ᢘᡭᢇ ᢘᡬᡬᡨ'],
     [z52tsagaantig, ' ᢚᡪᡱᡱᡪᡧ ᢘᡬᡬᡨ'],
   ];
 }
